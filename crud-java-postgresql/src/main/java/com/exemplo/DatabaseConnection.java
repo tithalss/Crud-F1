f5849a1bdbd9b2801a0b921b4c0ccc13d5aa9ce6
Thales <118466161+tithalss@users.noreply.github.com>
2024-09-12 16:38:05 -0300

    Update DatabaseConnection.java

--- crud-java-postgresql/src/main/java/com/exemplo/DatabaseConnection.java
+++ crud-java-postgresql/src/main/java/com/exemplo/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/crudapp";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "carrapato";
+    private static final String PASSWORD = "senha";
 
     public static Connection getConnection() throws SQLException {
         try {
